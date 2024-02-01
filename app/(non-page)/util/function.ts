11c66255eb82cbe91c07e00436ea1ffc9c712052
RHUK2 <gusdnr814@naver.com>
2024-02-01 10:06:18 +0900

    #FEAT 마크다운 컨텐츠 변환을 위한 라이브러리 및 함수 작성
    + unified, remark, rehype: 마크다운을 HTML 형식으로 변환
    + gray-matter: 메타 데이터 추출을 위함

---
+++ app/(non-page)/util/function.ts
@@ -0,0 +1,58 @@
+import { readFile, readFileSync, readdirSync } from 'fs';
+import matter from 'gray-matter';
+import rehypeSlug from 'rehype-slug';
+import rehypeStringify from 'rehype-stringify';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
+
+export function getDirectoryList() {
+  try {
+    const directoryList = readdirSync(`${process.cwd()}/public/Markdown`);
+
+    return directoryList;
+  } catch (err) {
+    console.log(err);
+
+    return [];
+  }
+}
+
+export function getPostList(directory: string) {
+  try {
+    const postList = readdirSync(`${process.cwd()}/public/Markdown/${directory}`);
+
+    const result = postList
+      .filter((post) => /.*.md/.test(post))
+      .map((post) => {
+        const content = readFileSync(`${process.cwd()}/public/Markdown/${directory}/${post}`, 'utf8');
+        const { data } = matter(content);
+        return data;
+      });
+
+    return result;
+  } catch (err) {
+    console.error(err);
+
+    return [];
+  }
+}
+
+export function getPost(directory: string, title: string) {
+  try {
+    const post = readFileSync(`${process.cwd()}/public/Markdown/${directory}/${title}.md`, 'utf8');
+
+    const result = unified()
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeSlug)
+      .use(rehypeStringify)
+      .processSync(matter(post).content);
+
+    return result.value;
+  } catch (err) {
+    console.error(err);
+
+    return '';
+  }
+}
