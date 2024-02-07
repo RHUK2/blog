0779d458404f6b590a1f5bdf1f477256a6f4ac22
RHUK2 <gusdnr814@naver.com>
2024-02-07 19:00:10 +0900

    #FEAT  다크모드 새로고침 시 FOUC 이슈 해결
    돔 로드 전 "동기적" 스크립트 삽입으로 HTML을 모두 파싱하기 전에 로컬스토리지를 읽어서 class를 삽입하는 방식으로 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Footer, Header } from "@/component";
 import "@/globals.css";
-import "github-markdown-css";
 import "highlight.js/styles/github-dark.css";
+// import "github-markdown-css";
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 
@@ -28,9 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${(inter.variable, roboto_mono.variable)} font-mono`}>
+      <body
+        className={`${(inter.variable, roboto_mono.variable)} bg-white font-mono dark:bg-stone-900 dark:text-stone-200`}
+      >
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="./script.js" />
         <Header />
-        <main className="m-auto min-h-screen min-w-[320px] max-w-[1024px] pt-12 shadow-2xl">
+        <main className="m-auto min-h-screen min-w-[320px] max-w-[1024px] pt-12 shadow-2xl dark:bg-stone-800 dark:shadow-stone-600">
           {children}
         </main>
         <Footer />
