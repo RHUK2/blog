bbf24b43d042e89d5dd9a336911862cacc38b0ab
RHUK2 <gusdnr814@naver.com>
2024-09-11 18:30:32 +0900

    #FIX 컬러 모드 판별 스크립트 인라인으로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,21 +32,42 @@ export const metadata: Metadata = {
   title: 'RHUK2',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+}
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang='en'>
-      <head>{/* <script async type='module' src='/script/dom_init.mjs'></script> */}</head>
+      <head>
+        <script
+          async
+          type='module'
+          dangerouslySetInnerHTML={{
+            __html: `
+            (() => { 
+              // FOUC(Flash of Unstyled Content) 방지를 위한 동기 스크립트 삽입
+              // Hydration 에러 발생할 수 밖에 없음
+              try {
+                if ( localStorage.theme === 'dark' || (!(localStorage.theme === '') && window.matchMedia('(prefers-color-scheme: dark)').matches)) 
+                {
+                  document.documentElement.classList.add('dark');
+                } else {
+                  document.documentElement.classList.remove('dark');
+                }
+              } catch (_) {}
+            })();
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${(inter.variable, roboto_mono.variable, pretendard.variable)} font-pret
         dark:bg-gray-950 dark:text-white`}>
         <Header />
         <main
           className='m-auto min-h-screen min-w-[320px] max-w-[1024px] 
-        border-x border-x-gray-200 bg-white pt-12 
+          border-x border-x-gray-200 bg-white pt-12 
         dark:border-x-gray-800 dark:bg-gray-950 '>
           {children}
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import { getTagList, readMarkdownMetaDataList } from './_util';
-
-export default function HomePage() {
+export default function RootPage() {
   return (
     <section className='m-auto min-w-[320px] max-w-[768px] px-4 py-10'>
       <h1>나를 소개해주세요.</h1>
