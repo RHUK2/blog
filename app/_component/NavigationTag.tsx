296f44cc619693cdb12df96548e20a02a1c4ce08
RHUK2 <gusdnr814@naver.com>
2024-09-12 09:32:04 +0900

    #FIX archive 페이지, NavigationTag 컴포넌트 데이터 바인딩

---
+++ app/_component/NavigationTag.tsx
@@ -0,0 +1,26 @@
+import { readTagList } from '@/_util';
+import Link from 'next/link';
+
+interface NavigationTagProps {
+  currentTag?: string;
+}
+
+export async function NavigationTag({ currentTag }: NavigationTagProps) {
+  const tagList = await readTagList();
+
+  return (
+    <nav className='flex flex-wrap items-center gap-2'>
+      {tagList.map((tag, index) => (
+        <Link
+          href={tag.name ? `/archive?tag=${tag.name}` : '/archive'}
+          key={`tag_${index}`}
+          className={`rounded-xl border border-gray-200 bg-gradient-to-br from-gray-100 from-30% to-gray-50 to-70% px-2 py-1 
+            text-xs font-medium 
+            dark:border-gray-700 dark:from-gray-900 dark:to-gray-800  
+            ${currentTag === tag.name ? 'outline outline-1' : (currentTag == null || currentTag === '') && tag.name === '' ? 'outline outline-1' : ''}`}>
+          {`${tag.name || 'ALL'} (${tag.postCount})`}
+        </Link>
+      ))}
+    </nav>
+  );
+}
--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,49 +1,30 @@
-import { Divider, Navigation } from '@/_component';
+import { Divider, NavigationTag, PostCard } from '@/_component';
 import { Pagination } from '@/_component/client';
-import { FOLDER_NAME, getPostList } from '@/_util';
-import Link from 'next/link';
+import { PAGE_SIZE, readPostList } from '@/_util';
 
-interface PostListPageProps {
+interface ArchivePageProps {
   params: {};
   searchParams: {
-    directory?: string;
     tag?: string;
     page?: string;
+    size?: string;
   };
 }
 
-const PAGE_SIZE = 10;
-
-export default async function PostListPage({ params, searchParams }: PostListPageProps) {
-  const postList = await getPostList(searchParams.directory);
-
-  const filteredPostList = postList.filter((post, index) => {
-    const page = searchParams.page == null || searchParams.page === '' ? 0 : parseInt(searchParams.page);
-    const previousPage = (page - 1) * PAGE_SIZE + PAGE_SIZE;
-    const currentPage = page * PAGE_SIZE + PAGE_SIZE;
-
-    return index >= previousPage && index < currentPage;
-  });
+export default async function ArchivePage({ params, searchParams }: ArchivePageProps) {
+  const postList = await readPostList(searchParams.tag, searchParams.page, searchParams.size);
 
   return (
     <>
       <section className='m-auto flex min-w-[320px] max-w-[768px] flex-col gap-8 px-4 py-10'>
-        <Navigation current={searchParams.tag} />
+        <NavigationTag currentTag={searchParams.tag} />
 
         <Divider />
 
-        <Pagination totalCount={postList.length} size={PAGE_SIZE} />
+        <Pagination totalCount={postList.totalCount} size={parseInt(PAGE_SIZE)} />
 
-        {filteredPostList.map((post, index) => (
-          <Link
-            key={`post_${index}`}
-            href={`/${FOLDER_NAME}/${post.directory}/${post.fileName}`}
-            className='min-h-28 rounded-lg border border-gray-200 bg-gradient-to-br from-gray-100 from-30% to-gray-50 to-70% p-4
-            hover:relative hover:-top-2 dark:border-gray-700 dark:from-gray-900 dark:to-gray-800'>
-            <span className='block'>{post.title ?? '-'}</span>
-            <span className='block'>{post.description ?? '-'}</span>
-            <span className='block'>{new Date(post.updatedAt).toUTCString() ?? '-'}</span>
-          </Link>
+        {postList.list.map((post, index) => (
+          <PostCard key={`post_${index}`} post={post} />
         ))}
       </section>
     </>
