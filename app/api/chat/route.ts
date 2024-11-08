999ad4d7c22adce7a29a7cd40b8cf764025a555c
RHUK2 <gusdnr814@naver.com>
2024-11-08 13:42:02 +0900

    #UPDATE

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,6 +16,9 @@ export async function POST(request: Request) {
     return Response.json({ chat: completion.choices[0].message });
   } catch (error) {
     console.error(error);
+    // 클라이언트 요청이 잘못된 경우
+    // return Response.json({ code: 'CLIENT_ERR' }, { status: 400 });
+    // 예측 불가능한 에러
     return Response.json({ code: 'SERVER_ERR' }, { status: 500 });
   }
 }
