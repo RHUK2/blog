87fdfafebb483348043d3cd2312de9603a8fe750
RHUK2 <gusdnr814@naver.com>
2025-01-23 18:30:33 +0900

    #FIX textarea auto-resizing 로직 변경
    - 포커스 시 max-height 계산해서 지정
    - 입력 필드에 사용자 입력 시, 키 업 시에 높이 계산 로직 적용

--- app/_clientComponent/Textarea.tsx
+++ app/_clientComponent/Textarea.tsx
@@ -1,39 +1,51 @@
 'use client';
 
-import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
+import { DetailedHTMLProps, forwardRef, TextareaHTMLAttributes, useImperativeHandle, useRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export const Textarea = forwardRef(function Textarea(
   {
-    rows,
     className,
     onChange,
-    ...TextareaProps
-  }: React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>,
+    onFocus,
+    onKeyUp,
+    ...textareaProps
+  }: DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>,
   ref,
 ) {
-  const textareaRef = useRef(null);
-
-  const [lines, setLines] = useState(1);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
   useImperativeHandle(ref, () => textareaRef.current);
 
   return (
     <textarea
       ref={textareaRef}
-      rows={lines}
-      onChange={(e) => {
-        const renderedLines = Math.floor(e.target.scrollHeight / parseFloat(getComputedStyle(e.target).lineHeight));
+      onFocus={(event) => {
+        const maxHeight = parseInt(getComputedStyle(event.target).lineHeight) * 7;
 
-        setLines(e.target.value ? Math.min(e.target.value.split('\n').length, renderedLines) : 1);
+        event.target.style.setProperty('max-height', `${maxHeight}px`);
 
-        if (onChange) onChange(e);
+        if (onFocus) onFocus(event);
+      }}
+      onChange={(event) => {
+        event.target.style.setProperty('height', 'auto');
+        event.target.style.setProperty('height', `${event.target.scrollHeight + event.target.clientTop * 2}px`);
+        event.target.scrollTop = event.target.scrollHeight;
+
+        if (onChange) onChange(event);
+      }}
+      onKeyUp={(event) => {
+        if (event.currentTarget.value === '') {
+          event.currentTarget.style.setProperty('height', 'auto');
+        }
+
+        if (onKeyUp) onKeyUp(event);
       }}
       className={twMerge(
-        'resize-none rounded-md border border-gray-400 bg-gradient-to-br from-gray-50 to-gray-100 p-2 dark:border-gray-700 dark:from-gray-900 dark:to-gray-800',
+        'resize-none rounded-md border border-gray-400 bg-transparent bg-gradient-to-br from-gray-50 to-gray-100 px-3 py-2 dark:border-gray-700 dark:from-gray-900 dark:to-gray-800',
         `${className ?? ''}`,
       )}
-      {...TextareaProps}
+      {...textareaProps}
     />
   );
 });
