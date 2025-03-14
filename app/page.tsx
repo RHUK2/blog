65a19c9eb8d279f63fbcaf1ac6f323cd118fe5b6
RHUK2 <gusdnr814@naver.com>
2025-03-14 18:01:56 +0900

    #FIX [/ page] 레이아웃 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { projectList } from './_data';
-import { CareerContentItem, CategoryBox, Profile, ProjectContentItem } from './_serverComponent';
+import { careerList, projectList, skillList } from './_data';
+import { CareerContentItem, CategoryBox, IcondBadge, Profile, ProjectContentItem } from './_serverComponent';
 
 export default function RootPage() {
   return (
@@ -7,16 +7,30 @@ export default function RootPage() {
       <section className='m-auto flex max-w-[768px] min-w-[320px] flex-col gap-12 px-4 py-10'>
         <Profile />
 
-        <CategoryBox category='기술 스택'></CategoryBox>
+        <CategoryBox category='기술 스택'>
+          <div className='flex flex-wrap gap-2'>
+            {skillList.map((skill) => (
+              <IcondBadge key={skill.text} icon={skill.icon} text={skill.text} />
+            ))}
+          </div>
+        </CategoryBox>
 
         <CategoryBox category='경력 • 프로젝트'>
-          <CareerContentItem />
+          {careerList.map((career) => (
+            <ul key={career.company} className='flex flex-col gap-8'>
+              <CareerContentItem data={career} />
+              {career.projectList.map((project) => (
+                <ProjectContentItem key={project.title} data={project} />
+              ))}
+            </ul>
+          ))}
+        </CategoryBox>
+
+        <CategoryBox category='개인 프로젝트'>
           {projectList.map((project) => (
             <ProjectContentItem key={project.title} data={project} />
           ))}
         </CategoryBox>
-
-        <CategoryBox category='개인 프로젝트'></CategoryBox>
 
         <CategoryBox category='학력'></CategoryBox>
       </section>
