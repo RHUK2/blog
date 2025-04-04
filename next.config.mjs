943cb7ddd7cc5e249f1ad2c3d3fd18e2715434ec
RHUK2 <gusdnr814@naver.com>
2025-04-04 14:28:11 +0900

    #FIX turbopack 설정 제거

--- next.config.mjs
+++ next.config.mjs
@@ -11,16 +11,16 @@ const nextConfig = {
     });
     return config;
   },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
+  // experimental: {
+  //   turbo: {
+  //     rules: {
+  //       '*.svg': {
+  //         loaders: ['@svgr/webpack'],
+  //         as: '*.js',
+  //       },
+  //     },
+  //   },
+  // },
 };
 
 export default nextConfig;
