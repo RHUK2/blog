373ce18c822b80e6291954b1bc1dd56f0c2b1ae4
RHUK2 <gusdnr814@naver.com>
2025-04-21 18:28:53 +0900

    #FIX jest svgr config 방식 수정

--- jest.config.ts
+++ jest.config.ts
@@ -13,22 +13,10 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.svg$': '<rootDir>/__mocks__/svg.js',
+    '^.+\\.(svg)$': '<rootDir>/__mocks__/svg.js',
     '^@/public/(.*)$': '<rootDir>/public/$1',
     '^@/(.*)$': '<rootDir>/app/$1',
   },
 };
 
-const configForSvgr = async () => {
-  const jestConfig = await createJestConfig(config)();
-
-  return {
-    ...jestConfig,
-    moduleNameMapper: {
-      '\\.(svg)$': '<rootDir>/__mocks__/svg.js',
-      ...jestConfig.moduleNameMapper,
-    },
-  };
-};
-
-export default configForSvgr;
+export default createJestConfig(config);
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,7 @@
 import '@testing-library/jest-dom';
 global.scrollTo = jest.fn();
+global.ResizeObserver = jest.fn().mockReturnValue({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+});
