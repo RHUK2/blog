53aece7920bb05dfc1553fe1f8eb55ace767c006
RHUK2 <gusdnr814@naver.com>
2025-04-22 19:01:03 +0900

    #FIX 정적데이터 수정

--- app/_data/staticData.ts
+++ app/_data/staticData.ts
@@ -51,7 +51,7 @@ export const careerList: TCareerList = [
     description: 'B2B HR 평판조회 서비스 회사',
     projectList: [
       {
-        title: '랜딩페이지 개발',
+        title: '신규 랜딩페이지 개발',
         contributions: 100,
         startDate: '2025.01.',
         endDate: '2025.02.',
@@ -72,7 +72,7 @@ export const careerList: TCareerList = [
           },
         ],
         experienceList: [
-          { behavior: '시멘틱 HTML 준수, Meta Tag 관리, sitemap 생성', result: 'SEO 최적화' },
+          { behavior: '시멘틱 HTML 준수, Meta Tag 관리, Sitemap 생성', result: 'SEO 최적화' },
           {
             behavior: '마크다운을 s3에 업로드하여 소개 페이지를 구성하고 공지글을 올릴 수 있도록 구현',
             result: '비개발자도 내용을 수정할 수 있어 업무 효율 개선',
@@ -87,7 +87,7 @@ export const careerList: TCareerList = [
       },
       {
         title: '사내 업무 보조툴 개발',
-        contributions: 70,
+        contributions: 100,
         startDate: '2024.09.',
         endDate: '2024.10.',
         description:
@@ -124,7 +124,7 @@ export const careerList: TCareerList = [
         ],
       },
       {
-        title: '체커 피드백 서비스 개발 • 유지보수',
+        title: '체커 피드백 신규 서비스 개발 • 유지보수',
         contributions: 70,
         startDate: '2023.10.',
         endDate: '2024.08.',
@@ -152,25 +152,37 @@ export const careerList: TCareerList = [
           },
         ],
         experienceList: [
-          { behavior: 'Axios Interceptor에서 리액트 훅을 사용가능하도록 변경', result: 'result' },
+          {
+            behavior:
+              '다면진단을 응답 중인 경우 주기적으로 세션 연장, 추가로 세션 연장 버튼을 만들어 세션 만료 시간을 수동으로 연장할 수 있도록 구현',
+            result: '사용자가 응답 중에는 세션이 만료되지 않도록 구현하여 사용자 경험 향상',
+          },
+          {
+            behavior: '배포 스크립트를 작성하여 배포 중 서버 다운타임 감소',
+            result:
+              '빌드가 완료된 후 결과물을 교체하는 방식으로 기존에 빌드 중에는 접근할 수 없던 이슈를 해결하여 사용자 경험 향상',
+          },
+          { behavior: '반응형 개발', result: '모바일 화면 대응을 통한 사용자 경험 향상' },
+          { behavior: 'Axios Instance를 Context API로 공유', result: '' },
           {
             behavior:
               '커스텀 에러를 이용하여 API 요청에 대한 예외 처리를 구분지어 관리, 처리되지 않은 에러는 Sentry로 전송',
-            result: 'result',
+            result: '',
           },
-          { behavior: '세션 연장 버튼을 추가하여 세션 만료 시간을 연장할 수 있도록 구현', result: 'result' },
-          { behavior: 'Next.js의 Middleware를 이용한 인증 처리', result: 'result' },
-          { behavior: 'PDFKit을 이용해 다면진단 보고서 구현', result: 'result' },
-          { behavior: 'NPM Workspace를 이용한 Monorepo 적용', result: 'result' },
-          { behavior: 'next-translate를 이용한 다국어 적용', result: 'result' },
-          { behavior: 'Cloudflare Turnstile 적용', result: 'result' },
-          { behavior: '기능 정리 및 api 문서 작성', result: 'result' },
-          { behavior: '프로젝트 설계 및 컨벤션 작성', result: 'result' },
+          { behavior: '프로젝트 초기 환경 설정, NPM Workspace를 이용한 Monorepo 적용, 라우터 구조 설계', result: '' },
+          { behavior: '디자인 화면과 기능을 바탕으로 백엔드와 협업하여 초기 API 문서 작성', result: '' },
+          { behavior: 'Next.js의 Middleware를 이용한 인증 • 권한 처리', result: '' },
+          {
+            behavior: 'Node.js 환경에서 PDFKit을 이용해 다면진단 보고서 구현',
+            result: '',
+          },
+          { behavior: 'Next-Translate를 이용한 다국어 적용', result: '' },
+          { behavior: 'Cloudflare Turnstile 적용', result: '' },
         ],
       },
       {
         title: '헌터스 서비스 리뉴얼 • 유지보수',
-        contributions: 70,
+        contributions: 80,
         startDate: '2022.09.',
         endDate: '2023.09.',
         description: '인사담당자와 헤드헌터 중개 서비스',
@@ -193,15 +205,32 @@ export const careerList: TCareerList = [
           },
         ],
         experienceList: [
-          { behavior: 'react-intl을 이용한 다국어 적용', result: 'result' },
-          { behavior: '기존 jQuery로 되어있던 서비스를 React로 리뉴얼', result: 'result' },
-          { behavior: '인사담당자, 협업부서, 헤드헌터, 어드민별 권한을 구분하여 접근 제어', result: 'result' },
-          { behavior: 'useDebounce, useThrottle 훅을 이용한 호출 주기 제어', result: 'result' },
+          { behavior: 'JQuery로 되어있던 서비스를 React로 리뉴얼', result: '유지보수와 추가 기능 개발이 용이해짐' },
+          {
+            behavior: '약 20개의 입력이 들어가는 폼과 유효성 검사 구현',
+            result: 'React-Hook-Form을 이용한 비제어 컴포넌트 방식으로 렌더링 최적화',
+          },
+          {
+            behavior: 'useDebounce, useThrottle 훅을 이용한 호출 주기 제어',
+            result: '데이터가 두 번 생성되는 이슈를 방지하여 사용자 경험 향상',
+          },
+          {
+            behavior: '배포 스크립트를 작성하여 배포 중 서버 다운타임 감소',
+            result:
+              '빌드가 완료된 후 결과물을 교체하는 방식으로 기존에 빌드 중에는 접근할 수 없던 이슈를 해결하여 사용자 경험 향상',
+          },
+          {
+            behavior: '아이콘 폰트 사용 시 초기 자원 로딩 중에 아이콘 대신 텍스트가 보이는 이슈 해결',
+            result: 'Layout Shift를 제거함으로써 사용자 경험 향상',
+          },
+          { behavior: '반응형 개발', result: '모바일 화면 대응을 통한 사용자 경험 향상' },
+          { behavior: '인사담당자, 협업부서, 헤드헌터, 어드민별 권한을 구분하여 접근 제어', result: '' },
+          { behavior: 'React-Intl을 이용한 다국어 적용', result: '' },
         ],
       },
       {
-        title: '체커 블랙 서비스 개발',
-        contributions: 50,
+        title: '체커 블랙 신규 서비스 개발',
+        contributions: 40,
         startDate: '2022.05.',
         endDate: '2022.08.',
         description: '인재 DB 서비스',
@@ -228,10 +257,16 @@ export const careerList: TCareerList = [
           },
         ],
         experienceList: [
-          { behavior: 'Typescript 도입', result: 'result' },
-          { behavior: 'React Query를 이용한 비동기 데이터 관리 도입', result: 'result' },
-          { behavior: 'React Hook Form을 이용한 폼 데이터 관리 도입', result: 'result' },
-          { behavior: '배포 스크립트를 작성하여 배포 중 서버 다운타임 감소', result: 'result' },
+          { behavior: 'Typescript 도입', result: '타입 안정성을 통한 예상치 못한 버그 발생 방지' },
+          {
+            behavior: 'React-Query를 이용한 비동기 데이터 관리 도입',
+            result: '기존 Redux-Saga의 많은 보일러 플레이트를 줄이기 위해 도입',
+          },
+          {
+            behavior: 'React-Hook-Form을 이용한 폼 데이터 관리 도입',
+            result: '비제어 컴포넌트 방식으로 렌더링 최적화',
+          },
+          { behavior: '반응형 개발', result: '모바일 화면 대응을 통한 사용자 경험 향상' },
         ],
       },
       {
@@ -255,9 +290,9 @@ export const careerList: TCareerList = [
           },
         ],
         experienceList: [
-          { behavior: '토큰 만료 시 토큰 재발급 후 이전 실패된 요청에 대해 재요청 하는 로직 적용', result: 'result' },
-          { behavior: '유저 동시접속 시 무한로딩되는 이슈 수정', result: 'result' },
-          { behavior: 'Google reCaptcha 적용', result: 'result' },
+          { behavior: '토큰 만료 시 토큰 재발급 후 이전 실패된 요청에 대해 재요청 하는 로직 적용', result: '' },
+          { behavior: 'CORS 에러 해결', result: '' },
+          { behavior: 'Google reCaptcha 적용', result: '' },
         ],
       },
     ],
