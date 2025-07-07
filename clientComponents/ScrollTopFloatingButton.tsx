0829828ce959d5399db46085a7148d0f2c29c80e
RHUK2 <gusdnr814@naver.com>
2025-07-07 18:40:45 +0900

    #FIX ScrollTopFloatingButton 요소 기준으로 위치 고정되도록 수정

--- clientComponents/ScrollTopFloatingButton.tsx
+++ clientComponents/ScrollTopFloatingButton.tsx
@@ -9,17 +9,19 @@ export function ScrollTopFloatingButton() {
   }
 
   return (
-    <motion.div
-      onClick={scrollTop}
-      className='fixed right-10 bottom-10 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gradient-to-br from-gray-100 from-30% to-gray-50 to-70% ring-2 dark:from-gray-900 dark:to-gray-800 dark:shadow-gray-700'
-      whileHover={{
-        scale: 1.1,
-      }}
-      whileFocus={{
-        scale: 1.1,
-      }}
-    >
-      <ChevronUp />
-    </motion.div>
+    <div className='sticky bottom-8 flex justify-end'>
+      <motion.button
+        onClick={scrollTop}
+        className='flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-gradient-to-br from-gray-100 from-30% to-gray-50 to-70% ring-2 dark:from-gray-900 dark:to-gray-800 dark:shadow-gray-700'
+        whileHover={{
+          scale: 1.1,
+        }}
+        whileFocus={{
+          scale: 1.1,
+        }}
+      >
+        <ChevronUp />
+      </motion.button>
+    </div>
   );
 }
