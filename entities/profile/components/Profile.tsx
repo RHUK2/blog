242341d4aca0bbd827ba7d53d0c5d9420e8afcc6
RHUK2 <gusdnr814@naver.com>
2025-12-12 16:16:53 +0900

    feat: nextjs 보안 취약점 업데이트, jest/playwright 삭제

--- entities/profile/components/Profile.tsx
+++ entities/profile/components/Profile.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Badge, SvgrIcon } from '@/shared/components';
 import Image from 'next/image';
 import Link from 'next/link';
--- jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-global.scrollTo = jest.fn();
-global.ResizeObserver = jest.fn().mockReturnValue({
-  observe: jest.fn(),
-  unobserve: jest.fn(),
-  disconnect: jest.fn(),
-});
